6d3ae667b5fb8d3514c1fc3d3e2c5986f09dedc3
gaojunliang <bjgjl@163.com>
2015-12-16 13:03:26 +0800

    修改支付宝为默认支付

--- wap/js/flowcheck.js
+++ wap/js/flowcheck.js
@@ -106,8 +106,8 @@ function Bindpay(){
 			 window.location.href='/wap/login.html';
 		 }else{
 			 //var payhtml='<span><input class="pay" type="radio"  id="payid1" name="payid" onclick="loadprice();" value="20">支付宝支付</span>';
-			 var payhtml = '<span><input class="pay" type="radio" checked id="payid3" name="payid" onclick="loadprice();" value="61"><img src="http://baidupaymentgateway.d1.cn/images/baidu_pay.png" /></span>';
-			 payhtml += '<br/><br/><span><input class="pay" type="radio"  id="payid1" name="payid" onclick="loadprice();" value="20">支付宝支付</span>';
+			 var payhtml = '<span><input class="pay" type="radio"  id="payid3" name="payid" onclick="loadprice();" value="61"><img src="http://baidupaymentgateway.d1.cn/images/baidu_pay.png" /></span>';
+			 payhtml += '<br/><br/><span><input class="pay" type="radio" checked  id="payid1" name="payid" onclick="loadprice();" value="20">支付宝支付</span>';
 			 if(json.ifCanHF){
 				 payhtml+='<span><input class="pay" type="radio" id="payid2" name="payid" onclick="loadprice();" value="0">货到付款</span>';
 			 }
