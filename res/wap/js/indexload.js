c113e513bb2d35d909c3d50b0bdcfb2c71bd661d
hq520it <525141335@qq.com>
2015-12-25 15:42:00 +0800

    wap首面轮播

--- res/wap/js/indexload.js
+++ res/wap/js/indexload.js
@@ -34,6 +34,62 @@ function loaddata(){
 		   });
 	}
 
+function banner(
+		sliderTs, //函数名
+		sliders,   //ul的ID的后面的数字
+		Pagenavi //分页按钮的ID
+		) {
+//$(Next).click(function(){sliderTs.next();});
+//$(Prev).click(function(){sliderTs.prev();});
+
+var num = 0;
+num = $("#slider" + sliders).children("li").length;
+var html = "";
+html = "<a href='javascript:void(0);' {c}>{i}</a>";
+var lasthtml,
+thtml;
+lasthtml = "";
+
+var $imgH = $(".swipe img").height();
+$(".swipe li").height($imgH);
+
+for (var i = 0; i < num; i++) {
+thtml = "";
+if (i == 0) {
+    thtml = html.replace("{c}", "class='active' ");
+
+} else {
+    thtml = html.replace("{c}", "");
+}
+thtml = thtml.replace("{i}", i + 1);
+lasthtml += thtml;
+};
+$(Pagenavi).html(lasthtml);
+var active = 0,
+as = $(Pagenavi).find("a");
+for (var i = 0; i < as.length; i++) {
+(function() {
+    var j = i;
+    as[i].onclick = function() {
+        sliderTs.slide(j);
+        return false;
+    }
+})();
+
+}
+var sliderTs=new TouchSlider("slider" + sliders,{
+duration:600, 
+direction:0, 
+interval:3000,//间隔时间
+fullsize:true,
+autoplay:true,
+});
+sliderTs.on('before',function(m,n){
+as[m].className='';
+as[n].className='active';
+});	
+setTimeout(function(){sliderTs.resize();},100);
+};
     
 
     function showpm(json){
@@ -52,8 +108,7 @@ function loaddata(){
     	   $("#slider1").html(lblistsli);
     	   banner("slider1",//函数名
     			   "1",//ul的ID的后面的数字-----id="slider1"
-    			   "#pagenavi"//分页按钮的ID
-    			 
+    			   "#pagenavi"//分页按钮的ID   			 
     			   );
 //    	   var nav="<div>";
 //    	   for(var i=0;i<lblists.length;i++){
