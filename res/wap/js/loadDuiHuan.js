b4932b738c8cd9d1c555586dbbca5fe1c6124f51
hq520it <525141335@qq.com>
2016-01-12 21:14:15 +0800

    兑换码兑换写成公共模块集成到xhEditor编辑器里，方便在做专题时用

---
+++ res/wap/js/loadDuiHuan.js
@@ -0,0 +1,61 @@
+$chsku = $("#choose_sku");
+function dhok(obj) {
+	var dhcode = $("#dhcode").val();
+	if (dhcode == '') {
+		$(".reg_err").html("兑换码不能为空，请输入！");
+		return;
+	} else {
+		$.ajax({
+			type : "get",
+			dataType : "json",
+			url : '/ajax/wap/choosecart.jsp',
+			cache : false,
+			data : {
+				id : dhcode
+			},
+			error : function(XmlHttpRequest) {
+			},
+			success : function(json) {
+				if (json.code == 1) {
+					alert(json.message);
+				} else if (json.code == 0) {
+					$choosesku(dhcode, $chsku)
+
+				} else if (json.code == 2) {
+					$(obj).attr('attr', dhcode);
+					$.inCart(obj, {
+						ajaxUrl : '/ajax/wap/dhInCart.jsp'
+					});
+				} else if (json.code == 99) {
+					window.location.href = "/wap/login.html";
+				}
+			},
+			beforeSend : function() {
+			},
+			complete : function() {
+			}
+		});
+	}
+}
+
+function skuclose() {
+	$chsku.hide();
+};
+$choosesku = function(code, chsku) {
+	$.ajax({
+		type : "get",
+		dataType : "text",
+		url : '/ajax/wap/chooseskunew.jsp',
+		cache : false,
+		data : {
+			code : code
+		},
+		error : function(XmlHttpRequest) {
+			alert('错误');
+		},
+		success : function(data) {
+			chsku.html(data);
+			chsku.show();
+		}
+	});
+};
